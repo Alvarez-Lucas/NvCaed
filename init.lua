f7f9b222038d5582e164dc996718abd91750b707
Lucas Alvarez <lucasalvarez@pm.me>
2022-06-12 16:15:00 -0400

    Add Neovide Config

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
 require("caed.packer")
 require("caed.config.keymaps")
 require("caed.config.options")
+require("caed.config.neovide")
---
+++ lua/caed/config/neovide.lua
@@ -0,0 +1,6 @@
+-- Api Shorteners
+local vo = vim.opt
+local vc = vim.cmd
+-- let g:neovide_transparency=0.8
+-- vc("let g:neovide_transparency=0.8")
+vc("set guifont=Hack\\ NF:h15")
