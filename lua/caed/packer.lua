eb8fb368c9f1dae9d604755d0e68b72d3eec85bc
Lucas Alvarez <lucasalvarez@pm.me>
2022-06-12 16:37:45 -0400

    Update Plugins

--- lua/caed/packer.lua
+++ lua/caed/packer.lua
@@ -1,21 +1,43 @@
+-- api shorteners
 local fn = vim.fn
+local vc = vim.cmd
+
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
   packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
 end
 
 return require('packer').startup(function(use)
-  -- My plugins here
-  -- use 'foo1/bar1.nvim'
-  -- use 'foo2/bar2.nvim'
   use("wbthomason/packer.nvim") -- Packer can manage itself
-  use{"vimwiki/vimwiki", cmd = "VimwikiIndex"}
---   use{"nvim-telescope/telescope.nvim", requires = {{'nvim-lua/plenary.nvim'}}, cmd = "Telescope"}
+
+  -- Colorschemes
+  use {
+      "phha/zenburn.nvim",
+      config = function() require("zenburn").setup() end,
+      disable = true
+  }
+  use {"jacoborus/tender.vim", event = "BufWinEnter", config = "vc('colorscheme tender')"}
+
+  -- Vimwiki
+  use{"vimwiki/vimwiki", cmd = {"VimwikiIndex", "cd $HOME/vimwiki"}}
+
+  -- Telescope
   use{"nvim-telescope/telescope.nvim", requires = {{'nvim-lua/plenary.nvim'}}, cmd = "Telescope", config = function() require('caed.telescope-config') end}
 
---  use{"nvim-telescope/telescope.nvim", requires = {{'nvim-lua/plenary.nvim'}}, cmd = "Telescope", config = "require('caed.telescope-config)"}
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
+  -- NvimTree
+  use {
+    'kyazdani42/nvim-tree.lua',
+    config = function() require('caed.nvim-tree-config') end,
+    -- cmd = "NvimTreeToggle",
+    requires = {
+      'kyazdani42/nvim-web-devicons', -- optional, for file icon
+    },
+    tag = 'nightly' -- optional, updated every week. (see issue #1193)
+  }
+
+  -- LSP?
+
+  -- END
   if packer_bootstrap then
     require('packer').sync()
   end
