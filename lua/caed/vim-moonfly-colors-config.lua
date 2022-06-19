a15100d7370bba2e30fabbb76d2eecba575315d4
Lucas Alvarez <lucasalvarez@pm.me>
2022-06-19 04:08:21 -0400

    Add moonfly config

---
+++ lua/caed/vim-moonfly-colors-config.lua
@@ -0,0 +1,16 @@
+-- require("vim-moonfly-colors-config")
+vim.g.moonflyUnderlineMatchParen = 1 -- Love this, especially between ()
+vim.g.moonflyItalics = 0
+-- vim.g.moonflyCursorColor = 1 -- does not work
+-- vim.g.moonflyNormalFloat = 1 -- makes it black/transparent / invisible, need border lines
+vim.g.moonflyTransparent = 1 -- gonna try with something
+vim.g.moonflyWinSeparator = 2 -- makes it skinnier
+vim.opt.fillchars = { -- makes it thicker but not as thick as disable thing above
+	horiz = "━",
+	horizup = "┻",
+	horizdown = "┳",
+	vert = "┃",
+	vertleft = "┫",
+	vertright = "┣",
+	verthoriz = "╋",
+}
