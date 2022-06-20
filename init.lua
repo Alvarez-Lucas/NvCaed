d273105b13e7fb53065ebafa6a48efb50741c8ac
Lucas Alvarez <lucasalvarez@pm.me>
2022-06-20 03:54:23 -0400

    Fixed vimwiki

--- init.lua
+++ init.lua
@@ -49,6 +49,13 @@ vim.g.maplocalleader = " "
 -- { name = "DiagnosticSignHint", text = "" },
 -- { name = "DiagnosticSignInfo", text = "" },
 
+-- VimWiki global before initializing
+vim.cmd("let g:vimwiki_global_ext = 0")
+vim.cmd("let g:vimwiki_ext2syntax = {'.md': 'markdown', '.markdown': 'markdown', '.mdown': 'markdown'}")
+vim.cmd(
+	"let g:vimwiki_list = [{'path': '~/vimwiki', 'template_path': '~/vimwiki/templates/', 'template_default': 'default', 'syntax': 'markdown', 'ext': '.md', 'path_html': '~/vimwiki/site_html/', 'custom_wiki2html': 'vimwiki_markdown', 'template_ext': '.tpl'}]"
+)
+
 require("caed.packer-config")
 require("caed.config.keymaps")
 require("caed.config.options")
--- lua/caed/packer-config.lua
+++ lua/caed/packer-config.lua
@@ -66,14 +66,8 @@ return require("packer").startup(function(use)
 	use({
 		"vimwiki/vimwiki",
 		cmd = { "Vimwiki", "VimwikiIndex" },
-		config = function()
-			vim.cmd("let g:vimwiki_ext2syntax = {'.md': 'markdown', '.markdown': 'markdown', '.mdown': 'markdown'}")
-
-			vim.cmd(
-				"let g:vimwiki_list = [{'path': '~/vimwiki', 'template_path': '~/vimwiki/templates/', 'template_default': 'default', 'syntax': 'markdown', 'ext': '.md', 'path_html': '~/vimwiki/site_html/', 'custom_wiki2html': 'vimwiki_markdown', 'template_ext': '.tpl'}]"
-			)
-		end,
 	})
+
 	-- Telescope
 	use({
 		"nvim-telescope/telescope.nvim",
