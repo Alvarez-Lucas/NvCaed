9cf30bed3aaeba79c335ce19eb1577e2922813ae
Lucas Alvarez <lucasalvarez@pm.me>
2022-06-20 03:17:51 -0400

    Add neovide check for transparent background

--- lua/caed/vim-moonfly-colors-config.lua
+++ lua/caed/vim-moonfly-colors-config.lua
@@ -4,6 +4,12 @@ vim.g.moonflyItalics = 0
 -- vim.g.moonflyCursorColor = 1 -- does not work
 -- vim.g.moonflyNormalFloat = 1 -- makes it black/transparent / invisible, need border lines
 vim.g.moonflyTransparent = 1 -- gonna try with something
+-- disable transparent background if in neovide
+vim.cmd([[ 
+if exists('g:neovide')
+  let g:moonflyTransparent = 0
+en
+]])
 vim.g.moonflyWinSeparator = 2 -- makes it skinnier
 vim.opt.fillchars = { -- makes it thicker but not as thick as disable thing above
 	horiz = "━",
