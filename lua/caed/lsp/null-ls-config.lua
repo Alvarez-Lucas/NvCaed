f6edeed4314eb8b32b1d3eada2bfb0d07f95cc71
Lucas Alvarez <lucasalvarez@pm.me>
2022-11-21 01:33:29 -0500

    Add Formatting for vimwiki files

--- lua/caed/lsp/null-ls-config.lua
+++ lua/caed/lsp/null-ls-config.lua
@@ -8,15 +8,38 @@ local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 null_ls.setup({
 	sources = {
-		formatting.prettier,
+		formatting.prettier.with({ extra_filetypes = { "vimwiki" } }),
 		formatting.stylua,
 		formatting.black,
 		formatting.taplo,
 	},
 	-- configure format on save
 	on_attach = function(current_client, bufnr)
-		-- print("filetype fingers crossed x2")
+		-- print("filetype fingers crossed x3")
 		-- print(vim.bo.filetype)
+		-- vimwiki format on save
+		-- if vim.bo.filetype == "vimwiki" then
+		-- 	print("we made it to vimwiki")
+		-- 	vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+		-- 	vim.api.nvim_create_autocmd("BufWritePre", {
+		-- 		group = augroup,
+		-- 		buffer = bufnr,
+		-- 		callback = function()
+		-- 			vim.lsp.buf.format({
+		-- 				filter = function(client)
+		-- 					--  only use null-ls for formatting instead of lsp server
+		-- 					-- if vim.bo.filetype == "powershell" then
+		-- 					-- 	print("we in here bitches")
+		-- 					-- end
+		-- 					return client.name == "null_ls"
+		-- 				end,
+		-- 				bufnr = bufnr,
+		-- 			})
+		-- 		end,
+		-- 	})
+		-- end
+
+		-- All files auto format on save
 		if current_client.supports_method("textDocument/formatting") then
 			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
 			vim.api.nvim_create_autocmd("BufWritePre", {
