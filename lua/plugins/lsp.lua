2c2f797dfb6a7b46a99eeef1c42b8206d3df594b
Lucas Alvarez <lucasalvarez@pm.me>
2023-04-08 20:51:41 -0700

    feat(plugin): add telescope

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -86,9 +86,10 @@ return {
 					vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
 					vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, opts)
 					vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-					vim.keymap.set("n", "<space>f", function()
-						vim.lsp.buf.format({ async = true })
-					end, opts)
+					-- TODO make a keybind for this that doesn't interfere with telescope
+					-- vim.keymap.set("n", "<space>f", function()
+					--vim.lsp.buf.format({ async = true })
+					--end, opts)
 				end,
 			})
 		end,
