ad7fc2b11fe2389b210226ce2b83b1bca103e5e2
Lucas Alvarez <lucasalvarez@pm.me>
2023-04-08 22:35:46 -0700

    feat(telescope): add telescope-zf-native.nvim

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,55 @@
 return {
 	"nvim-telescope/telescope.nvim",
 	tag = "0.1.1",
-	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local actions = require("telescope.actions")
+		require("telescope").setup({
+			defaults = {
+				-- borderchars = { "█", " ", "▀", "█", "█", " ", " ", "▀" },
+				-- Default configuration for telescope goes here:
+				-- config_key = value,
+				mappings = {
+					i = {
+						["<esc>"] = actions.close,
+						["<C-u>"] = false,
+						["<C-j>"] = actions.move_selection_next,
+						["<C-k>"] = actions.move_selection_previous,
+					},
+				},
+			},
+			extensions = {
+				["zf-native"] = {
+					-- options for sorting file-like items
+					file = {
+						-- override default telescope file sorter
+						enable = true,
+
+						-- highlight matching text in results
+						highlight_results = true,
+
+						-- enable zf filename match priority
+						match_filename = true,
+					},
+
+					-- options for sorting all other items
+					generic = {
+						-- override default telescope generic item sorter
+						enable = true,
+
+						-- highlight matching text in results
+						highlight_results = true,
+
+						-- disable zf filename match priority
+						match_filename = false,
+					},
+				},
+			},
+		})
+
+		require("telescope").load_extension("zf-native")
+	end,
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"natecraddock/telescope-zf-native.nvim",
+	},
 }
