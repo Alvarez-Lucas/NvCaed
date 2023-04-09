d535c2a081d06dd38fe110add1adc6ee18fc2db0
Lucas Alvarez <lucasalvarez@pm.me>
2023-04-09 00:12:14 -0700

    feat(chadtree): add disabled autocmd to open tree instead of netrw

--- lua/plugins/chadtree.lua
+++ lua/plugins/chadtree.lua
@@ -3,6 +3,13 @@ return {
 	branch = "chad",
 	build = "python3 -m chadtree deps",
 	init = function()
+		-- vim.cmd([[
+		-- augroup Chad
+		-- autocmd!
+		-- autocmd StdinReadPre * let s:std_in=1
+		-- autocmd VimEnter * if argc() == 1 && isdirectory(argv()[0]) && !exists("s:std_in") | exe 'CHADopen' | wincmd p | ene | exe 'cd '.argv()[0] | endif
+		-- augroup END
+		-- ]])
 		vim.g.chadtree_settings = {
 			["keymap.primary"] = { "l", "<enter>" },
 			["options.close_on_open"] = true,
