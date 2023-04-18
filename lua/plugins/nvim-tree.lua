ca648cc55bfd383bfe7747e5d38e54c462319d52
Lucas Alvarez <lucasalvarez@pm.me>
2023-04-18 03:13:24 -0400

    feat(nvim-tree): configure keymaps and behavior
    add autoclose, add icons, add keymaps, etc

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,37 +1,217 @@
 return {
 	{
-
 		"nvim-tree/nvim-tree.lua",
 		version = "*",
 		dependencies = {
 			"nvim-tree/nvim-web-devicons",
 		},
 		config = function()
+			local api = require("nvim-tree.api")
+			-- auto close
+			local function tab_win_closed(winnr)
+				-- local api = require("nvim-tree.api")
+				local tabnr = vim.api.nvim_win_get_tabpage(winnr)
+				local bufnr = vim.api.nvim_win_get_buf(winnr)
+				local buf_info = vim.fn.getbufinfo(bufnr)[1]
+				local tab_wins = vim.tbl_filter(function(w)
+					return w ~= winnr
+				end, vim.api.nvim_tabpage_list_wins(tabnr))
+				local tab_bufs = vim.tbl_map(vim.api.nvim_win_get_buf, tab_wins)
+				if buf_info.name:match(".*NvimTree_%d*$") then -- close buffer was nvim tree
+					-- Close all nvim tree on :q
+					if not vim.tbl_isempty(tab_bufs) then -- and was not the last window (not closed automatically by code below)
+						api.tree.close()
+					end
+				else -- else closed buffer was normal buffer
+					if #tab_bufs == 1 then -- if there is only 1 buffer left in the tab
+						local last_buf_info = vim.fn.getbufinfo(tab_bufs[1])[1]
+						if last_buf_info.name:match(".*NvimTree_%d*$") then -- and that buffer is nvim tree
+							vim.schedule(function()
+								if #vim.api.nvim_list_wins() == 1 then -- if its the last buffer in vim
+									vim.cmd("quit") -- then close all of vim
+								else -- else there are more tabs open
+									vim.api.nvim_win_close(tab_wins[1], true) -- then close only the tab
+								end
+							end)
+						end
+					end
+				end
+			end
+
+			vim.api.nvim_create_autocmd("WinClosed", {
+				callback = function()
+					local winnr = tonumber(vim.fn.expand("<amatch>"))
+					vim.schedule_wrap(tab_win_closed(winnr))
+				end,
+				nested = true,
+			})
+
+			-- edit_or_open and vsplit_preview
+			-- local api = require("nvim-tree.api")
+			local function edit_or_open()
+				-- local api = require("nvim-tree.api")
+				local node = api.tree.get_node_under_cursor()
+
+				if node.nodes ~= nil then
+					-- expand or collapse folder
+					api.node.open.edit()
+				else
+					-- open file
+					api.node.open.edit()
+					-- Close the tree if file was opened
+					api.tree.close()
+				end
+			end
+
+			-- open as vsplit on current node
+			local function vsplit_preview()
+				local node = api.tree.get_node_under_cursor()
+
+				if node.nodes ~= nil then
+					-- expand or collapse folder
+					api.node.open.edit()
+				else
+					-- open file as vsplit
+					api.node.open.vertical()
+				end
+
+				-- Finally refocus on tree if it was lost
+				api.tree.focus()
+			end
+
+			-- on on_attach
+			local function on_attach(bufnr)
+				local api = require("nvim-tree.api")
+
+				local function opts(desc)
+					return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+				end
+
+				-- Default mappings. Feel free to modify or remove as you wish.
+				-- BEGIN_DEFAULT_ON_ATTACH
+				vim.keymap.set("n", "<C-]>", api.tree.change_root_to_node, opts("CD"))
+				vim.keymap.set("n", "<C-e>", api.node.open.replace_tree_buffer, opts("Open: In Place"))
+				vim.keymap.set("n", "<C-k>", api.node.show_info_popup, opts("Info"))
+				vim.keymap.set("n", "<C-r>", api.fs.rename_sub, opts("Rename: Omit Filename"))
+				vim.keymap.set("n", "<C-t>", api.node.open.tab, opts("Open: New Tab"))
+				vim.keymap.set("n", "<C-v>", api.node.open.vertical, opts("Open: Vertical Split"))
+				vim.keymap.set("n", "<C-x>", api.node.open.horizontal, opts("Open: Horizontal Split"))
+				vim.keymap.set("n", "<BS>", api.node.navigate.parent_close, opts("Close Directory"))
+				vim.keymap.set("n", "<CR>", api.node.open.edit, opts("Open"))
+				vim.keymap.set("n", "<Tab>", api.node.open.preview, opts("Open Preview"))
+				vim.keymap.set("n", ">", api.node.navigate.sibling.next, opts("Next Sibling"))
+				vim.keymap.set("n", "<", api.node.navigate.sibling.prev, opts("Previous Sibling"))
+				-- vim.keymap.set("n", ".", api.node.run.cmd, opts("Run Command"))
+				-- vim.keymap.set("n", "\\", api.node.run.cmd, opts("Run Command"))
+				vim.keymap.set("n", "-", api.tree.change_root_to_parent, opts("Up"))
+				vim.keymap.set("n", "a", api.fs.create, opts("Create"))
+				vim.keymap.set("n", "bmv", api.marks.bulk.move, opts("Move Bookmarked"))
+				vim.keymap.set("n", "B", api.tree.toggle_no_buffer_filter, opts("Toggle No Buffer"))
+				vim.keymap.set("n", "c", api.fs.copy.node, opts("Copy"))
+				vim.keymap.set("n", "C", api.tree.toggle_git_clean_filter, opts("Toggle Git Clean"))
+				vim.keymap.set("n", "[c", api.node.navigate.git.prev, opts("Prev Git"))
+				vim.keymap.set("n", "]c", api.node.navigate.git.next, opts("Next Git"))
+				vim.keymap.set("n", "d", api.fs.remove, opts("Delete"))
+				vim.keymap.set("n", "D", api.fs.trash, opts("Trash"))
+				vim.keymap.set("n", "E", api.tree.expand_all, opts("Expand All"))
+				vim.keymap.set("n", "e", api.fs.rename_basename, opts("Rename: Basename"))
+				vim.keymap.set("n", "]e", api.node.navigate.diagnostics.next, opts("Next Diagnostic"))
+				vim.keymap.set("n", "[e", api.node.navigate.diagnostics.prev, opts("Prev Diagnostic"))
+				vim.keymap.set("n", "F", api.live_filter.clear, opts("Clean Filter"))
+				vim.keymap.set("n", "f", api.live_filter.start, opts("Filter"))
+				vim.keymap.set("n", "g?", api.tree.toggle_help, opts("Help"))
+				vim.keymap.set("n", "gy", api.fs.copy.absolute_path, opts("Copy Absolute Path"))
+				-- vim.keymap.set("n", "H", api.tree.toggle_hidden_filter, opts("Toggle Dotfiles"))
+				vim.keymap.set("n", ".", api.tree.toggle_hidden_filter, opts("Toggle Dotfiles"))
+				vim.keymap.set("n", "I", api.tree.toggle_gitignore_filter, opts("Toggle Git Ignore"))
+				vim.keymap.set("n", "J", api.node.navigate.sibling.last, opts("Last Sibling"))
+				vim.keymap.set("n", "K", api.node.navigate.sibling.first, opts("First Sibling"))
+				vim.keymap.set("n", "m", api.marks.toggle, opts("Toggle Bookmark"))
+				-- vim.keymap.set("n", "o", api.node.open.edit, opts("Open"))
+				-- vim.keymap.set("n", "O", api.node.open.no_window_picker, opts("Open: No Window Picker"))
+				vim.keymap.set("n", "p", api.fs.paste, opts("Paste"))
+				vim.keymap.set("n", "P", api.node.navigate.parent, opts("Parent Directory"))
+				vim.keymap.set("n", "q", api.tree.close, opts("Close"))
+				vim.keymap.set("n", "r", api.fs.rename, opts("Rename"))
+				vim.keymap.set("n", "R", api.tree.reload, opts("Refresh"))
+				-- vim.keymap.set("n", "s", api.node.run.system, opts("Run System"))
+				-- vim.keymap.set("n", "S", api.tree.search_node, opts("Search"))
+				vim.keymap.set("n", "o", api.node.run.system, opts("Run System"))
+				vim.keymap.set("n", "O", api.tree.search_node, opts("Search"))
+				vim.keymap.set("n", "U", api.tree.toggle_custom_filter, opts("Toggle Hidden"))
+				vim.keymap.set("n", "W", api.tree.collapse_all, opts("Collapse"))
+				vim.keymap.set("n", "x", api.fs.cut, opts("Cut"))
+				vim.keymap.set("n", "y", api.fs.copy.filename, opts("Copy Name"))
+				vim.keymap.set("n", "Y", api.fs.copy.relative_path, opts("Copy Relative Path"))
+				vim.keymap.set("n", "<2-LeftMouse>", api.node.open.edit, opts("Open"))
+				vim.keymap.set("n", "<2-RightMouse>", api.tree.change_root_to_node, opts("CD"))
+				-- END_DEFAULT_ON_ATTACH
+
+				-- h, j, k, l Style Navigation And Editing
+				vim.keymap.set("n", "l", edit_or_open, opts("Edit Or Open"))
+				vim.keymap.set("n", "L", vsplit_preview, opts("Vsplit Preview"))
+				-- vim.keymap.set("n", "h", api.tree.close, opts("Close"))
+				vim.keymap.set("n", "h", api.node.navigate.parent_close, opts("Close"))
+				vim.keymap.set("n", "H", api.tree.collapse_all, opts("Collapse All"))
+
+				-- Mappings removed via:
+				--   remove_keymaps
+				--   OR
+				--   view.mappings.list..action = ""
+				--
+				-- The dummy set before del is done for safety, in case a default mapping does not exist.
+				--
+				-- You might tidy things by removing these along with their default mapping.
+				-- vim.keymap.set("n", "O", "", { buffer = bufnr })
+				-- vim.keymap.del("n", "O", { buffer = bufnr })
+				-- vim.keymap.set("n", "<2-RightMouse>", "", { buffer = bufnr })
+				-- vim.keymap.del("n", "<2-RightMouse>", { buffer = bufnr })
+				-- vim.keymap.set("n", "D", "", { buffer = bufnr })
+				-- vim.keymap.del("n", "D", { buffer = bufnr })
+				-- vim.keymap.set("n", "E", "", { buffer = bufnr })
+				-- vim.keymap.del("n", "E", { buffer = bufnr })
+
+				-- Mappings migrated from view.mappings.list
+				--
+				-- You will need to insert "your code goes here" for any mappings with a custom action_cb
+				vim.keymap.set("n", "A", api.tree.expand_all, opts("Expand All"))
+				vim.keymap.set("n", "?", api.tree.toggle_help, opts("Help"))
+				vim.keymap.set("n", "C", api.tree.change_root_to_node, opts("CD"))
+				vim.keymap.set("n", "P", function()
+					local node = api.tree.get_node_under_cursor()
+					print(node.absolute_path)
+				end, opts("Print Node Path"))
+
+				-- vim.keymap.set("n", "Z", api.node.run.system, opts("Run System"))
+			end
+
+			-- config
 			require("nvim-tree").setup({ -- BEGIN_DEFAULT_OPTS
 				auto_reload_on_write = true,
 				disable_netrw = false,
-				hijack_cursor = false,
+				hijack_cursor = false, -- default: false
 				hijack_netrw = true,
-				hijack_unnamed_buffer_when_opening = false,
+				hijack_unnamed_buffer_when_opening = false, -- default: false
 				sort_by = "name",
 				root_dirs = {},
-				prefer_startup_root = false,
+				prefer_startup_root = true, --default: false
 				sync_root_with_cwd = false,
 				reload_on_bufenter = false,
 				respect_buf_cwd = false,
-				on_attach = "default",
-				remove_keymaps = false,
+				on_attach = on_attach, -- default: "default"
+				remove_keymaps = true, -- default: "false"
 				select_prompts = false,
 				view = {
 					centralize_selection = false,
 					cursorline = true,
 					debounce_delay = 15,
-					width = 30,
+					-- width = 30,
+					width = 40,
 					hide_root_folder = false,
 					side = "left",
 					preserve_window_proportions = false,
-					number = false,
-					relativenumber = false,
+					number = true, -- default: false
+					relativenumber = true, -- default: false
 					signcolumn = "yes",
 					mappings = {
 						custom_only = false,
@@ -40,11 +220,11 @@ return {
 						},
 					},
 					float = {
-						enable = false,
+						enable = false, -- default: false
 						quit_on_focus_loss = true,
 						open_win_config = {
-							relative = "editor",
-							border = "rounded",
+							relative = "editor", -- default: editor
+							border = "rounded", -- default: rounded
 							width = 30,
 							height = 30,
 							row = 1,
@@ -53,17 +233,17 @@ return {
 					},
 				},
 				renderer = {
-					add_trailing = false,
+					add_trailing = true, -- default: false
 					group_empty = false,
-					highlight_git = false,
+					highlight_git = false, -- default: false
 					full_name = false,
 					highlight_opened_files = "none",
 					highlight_modified = "none",
 					root_folder_label = ":~:s?$?/..?",
 					indent_width = 2,
 					indent_markers = {
-						enable = false,
-						inline_arrows = true,
+						enable = true, -- default: false
+						inline_arrows = true, -- default: true
 						icons = {
 							corner = "└",
 							edge = "│",
@@ -74,7 +254,7 @@ return {
 					},
 					icons = {
 						webdev_colors = true,
-						git_placement = "before",
+						git_placement = "after", -- default: before
 						modified_placement = "after",
 						padding = " ",
 						symlink_arrow = " ➛ ",
@@ -101,13 +281,20 @@ return {
 								symlink_open = "",
 							},
 							git = {
-								unstaged = "✗",
+								-- unstaged = "✗",
+								unstaged = "",
 								staged = "✓",
+								-- staged = "S",
 								unmerged = "",
+								-- unmerged = "",
 								renamed = "➜",
-								untracked = "★",
+								-- renamed = "➜",
+								-- untracked = "★",
+								untracked = "U",
 								deleted = "",
+								-- deleted = "",
 								ignored = "◌",
+								-- ignored = "◌",
 							},
 						},
 					},
@@ -119,7 +306,7 @@ return {
 					auto_open = true,
 				},
 				update_focused_file = {
-					enable = false,
+					enable = true, -- default: false
 					update_root = false,
 					ignore_list = {},
 				},
@@ -128,7 +315,7 @@ return {
 					args = {},
 				},
 				diagnostics = {
-					enable = false,
+					enable = true, -- default: false
 					show_on_dirs = false,
 					show_on_open_dirs = true,
 					debounce_delay = 50,
@@ -147,13 +334,15 @@ return {
 					dotfiles = false,
 					git_clean = false,
 					no_buffer = false,
-					custom = {},
+					custom = {
+						"^\\.git",
+					},
 					exclude = {},
 				},
 				filesystem_watchers = {
 					enable = true,
 					debounce_delay = 50,
-					ignore_dirs = {},
+					ignore_dirs = { ".git" }, -- default: {}
 				},
 				git = {
 					enable = true,
@@ -188,12 +377,13 @@ return {
 						},
 					},
 					open_file = {
-						quit_on_open = false,
+						quit_on_open = true, -- default: false
 						resize_window = true,
 						window_picker = {
 							enable = true,
 							picker = "default",
-							chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+							-- chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+							chars = "asdfjkl;",
 							exclude = {
 								filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
 								buftype = { "nofile", "terminal", "help" },
@@ -229,7 +419,7 @@ return {
 				},
 				experimental = {
 					git = {
-						async = false,
+						async = true, -- default: false
 					},
 				},
 				log = {
