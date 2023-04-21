b5486dfd1f26594cde84562f80d9a8a236852f1f
Lucas Alvarez <lucasalvarez@pm.me>
2023-04-21 01:47:40 -0400

    feat(nvim-autopairs): add keybind <C-e>

--- lua/plugins/nvim-autopairs.lua
+++ lua/plugins/nvim-autopairs.lua
@@ -1,6 +1,17 @@
 return {
 	"windwp/nvim-autopairs",
 	config = function()
-		require("nvim-autopairs").setup()
+		require("nvim-autopairs").setup({
+			fast_wrap = {
+				map = "<C-e>",
+				chars = { "{", "[", "(", '"', "'" },
+				pattern = [=[[%'%"%>%]%)%}%,]]=],
+				end_key = "$",
+				keys = "qwertyuiopzxcvbnmasdfghjkl",
+				check_comma = true,
+				highlight = "Search",
+				highlight_grey = "Comment",
+			},
+		})
 	end,
 }
