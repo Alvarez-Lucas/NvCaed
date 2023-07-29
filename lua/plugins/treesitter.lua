2435113d75332ddaa684c6b2867beb8aae5e9caa
Lucas Alvarez <lucasalvarez@pm.me>
2023-07-28 22:19:11 -0400

    feat(treesitter): enable highlight, remove rainbowq, disable textsubjects

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -7,21 +7,15 @@ return {
 			require("nvim-treesitter.install").compilers = { "clang" }
 			require("nvim-treesitter.configs").setup({
 				ensure_installed = "all",
-				rainbow = {
+				highlight = {
 					enable = true,
-					-- list of languages you want to disable the plugin for
-					disable = { "jsx", "cpp" },
-					-- Which query to use for finding delimiters
-					query = "rainbow-parens",
-					-- Highlight the entire buffer all at once
-					strategy = require("ts-rainbow").strategy.global,
 				},
 			})
 		end,
-		dependencies = { "HiPhish/nvim-ts-rainbow2" },
 	},
 
 	{
+		enabled = false,
 		"RRethy/nvim-treesitter-textsubjects",
 		config = function()
 			require("nvim-treesitter.configs").setup({
