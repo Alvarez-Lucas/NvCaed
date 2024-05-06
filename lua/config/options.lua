a4212248c65aeb5ba5984c6cd83d5d185989ac12
GitButler <gitbutler@gitbutler.com>
2024-05-05 21:37:53 -0400

    GitButler Integration Commit
    This is an integration commit for the virtual branches that GitButler is tracking.
    
    Due to GitButler managing multiple virtual branches, you cannot switch back and
    forth between git branches and virtual branches easily. 
    
    If you switch to another branch, GitButler will need to be reinitialized.
    If you commit on this branch, GitButler will throw it away.
    
    Here are the branches that are currently applied:
     - main (refs/gitbutler/main)
       - lua/plugins/telescope.lua
       - lua/plugins/indent-blankline.lua
       - lua/plugins/nvim-tree.lua
       - init.lua
       - lua/plugins/code_runner.lua
       - lua/plugins/lsp-linting-formatting/none-ls-nvim.lua
       - lua/plugins/vimwiki.lua
       - lua/plugins/lsp-linting-formatting/nvim-lsp-config.lua
       - lua/plugins/lsp.lua
       - README.md
       - lua/plugins/vim-dadbod.lua
       - lua/plugins/colorschemes/selenized-nvim.lua
       - lua/plugins/lsp-linting-formatting/neodev-nvim.lua
       - test.txt
       - lua/plugins/todo-comments-nvim.lua
       - lua/plugins/neogit.lua
       - lua/plugins/colorschemes/newspaper-nvim.lua
       - lua/plugins/nvim-cmp.lua
       - lua/plugins/lsp-linting-formatting/auto-format-nvim.lua
       - lua/config/keymaps.lua
       - lua/plugins/nvim-surround.lua
       - lua/plugins/treesitter.lua
       - lua/plugins/lsp-linting-formatting/mason-nvim.lua
       - lua/plugins/nvim-autopairs.lua
       - lua/plugins/comment.lua
       - lua/plugins/nvim-ts-autotag.lua
       - lua/config/autocommands.lua
       - lua/plugins/gitsigns.lua
       - lazy-lock.json
       - lua/plugins/nvim-hlslens.lua
       - lua/plugins/nvim-scrollbar.lua
       - lua/plugins/neoscroll-nvim.lua
     - refactor/options (refs/gitbutler/refactor/options)
       branch head: 66fb721c045040a17dfbd6195760a725195f7cb4
       - lua/plugins/notifier-nvim.lua
       - lua/plugins/staline-nvim.lua
       - lua/config/options.lua
    
    Your previous branch was: refs/heads/main
    
    The sha for that commit was: 28808be0ed4b2d8cda925b51f597e27c371a63b7
    
    For more information about what we're doing here, check out our docs:
    https://docs.gitbutler.com/features/virtual-branches/integration-branch

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,8 +1,21 @@
--- Api Shorteners
 local vo = vim.opt
 local vc = vim.cmd
 
+-- TODO: Add more options from classic https://youtu.be/hY5-Q6NxQgY
+
 -- Options --
+--
+local options = {}
+
+for k, v in pairs(options) do
+	vim.opt[k] = v
+end
+
+-- Set clipboard for default register
+vim.opt.clipboard:append("unnamedplus")
+
+-- Removes the nvim splash screen
+vo.shortmess:append({ I = true })
 
 -- Buffer
 vo.hidden = true
@@ -13,6 +26,32 @@ vo.relativenumber = true
 
 -- Cursor Line
 vo.cursorline = true
+
+-- Sign Column
+vo.signcolumn = "yes"
+
+-- Colors
+vo.termguicolors = true
+
+-- Scroll off
+vo.scrolloff = 8
+vo.sidescrolloff = 8
+
+-- Disable Mouse Input
+vim.opt.mouse = ""
+
+-- Global Status Line
+vo.laststatus = 3
+
+-- Command Height
+-- vc([[set cmdheight=1]])
+vim.opt.cmdheight = 1
+
+-- Remove end of buffer character
+vim.opt.fillchars:append({ eob = " " })
+
+-- Make Keywords include "-"
+vim.opt.iskeyword:append("-")
 
 -- Indentation
 vo.shiftwidth = 2
@@ -26,51 +65,22 @@ vo.smartcase = true
 vo.hlsearch = true
 vo.incsearch = true
 
--- Sign Column
-vo.signcolumn = "yes"
-
--- Colors
-vo.termguicolors = true
-
--- Column Marker
--- vc("set colorcolumn=80")
-
--- Make Keywords include "-"
-vc("set iskeyword+=-")
-
--- Scroll off
-vo.scrolloff = 8
-vo.sidescrolloff = 8
-
--- Lightspeed disabled default maps
--- TODO: readd if using lightspeed
--- vim.g.lightspeed_no_default_keymaps = 0
-
--- Disable Mouse Input
-vc("set mouse=")
-
--- GUI Font
--- vc([[set guifont=JetBrainsMono\ NFM:h18]])
-vc([[set guifont=JetBrainsMono\ Nerd\ Font:h28]])
--- vc([[set guifont=JetBrainsMono\ NFM:h15]])
--- vc([[set guifont=Monaco\ NFM:h15]])
--- vc([[set guifont=Hack\ NFM:h15]])
-
--- Global Status Line
-vo.laststatus = 3
-
--- Command Height
-vc([[set cmdheight=1]])
-
--- Remove end of buffer character
-vim.opt.fillchars:append({ eob = " " })
-
 -- Temp Directory
 vc([[
 set directory=$HOME/.vim/tmp
 ]])
 
+-- GUI Font
+-- TODO: Move to neovide
+vc([[set guifont=JetBrainsMono\ Nerd\ Font:h28]])
+-- vc([[set guifont=JetBrainsMono\ NFM:h18]])
+-- vc([[set guifont=JetBrainsMono\ NFM:h15]])
+-- vc([[set guifont=Monaco\ NFM:h15]])
+-- vc([[set guifont=Hack\ NFM:h15]])
+
 -- Neovide
 -- km("n", "<A-left>", ":set guifont=JetBrainsMono\\ NF:h15<cr>", defaultOpts)
 -- vc([[set guifont=JetBrainsMono\ NF:h16]])
 -- vc([[set guifont=FantasqueSansM\ Nerd\ Font:h20]])
+-- Copy into system clipboard
+--
